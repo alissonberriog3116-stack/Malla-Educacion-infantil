c0ae45f111756444b3930e2ed5a837eec324d8f0
Alisson <alissonberriog3116@gmail.com>
2025-08-01 19:54:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -149,8 +149,8 @@ document.getElementById("add-materia").addEventListener("click", () => {
   renderMalla();
 });
 
+renderMalla();
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('service-worker.js');
 }
-
-renderMalla();
